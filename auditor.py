079bfc7d11d03426689dffd2bd671dd523ebbc27
punkKD <pungkahding@gmail.com>
2026-09-10 22:36:56 +0800

    Remove:choice. Update:Stocks Input.

--- auditor.py
+++ auditor.py
@@ -2,15 +2,13 @@ inventory=0
 stocks=0
 
 while True:
-    choice=input("Enter '1' to update inventory, '2' to check inventory, or 'q' to quit: ")
-    if choice == '1':
-        stocks = input("Enter the current stock count: ")
-        if stocks.isdigit():
-            inventory = int(stocks)
-            print("Inventory has been updated.")
-    elif choice == '2':
-        print(f"Current inventory: {inventory}")
-    elif choice == 'q' or choice =="quit":
-        break
+    stocks = input("Enter the current stock count or type quit to exit: ")
+    if stocks.isdigit():
+        inventory += int(stocks)
+        print("Inventory has been updated.")
     else:
-        print("Invalid choice. Please try again.")
+        if stocks == 'q' or stocks =="quit":
+            break
+        print("Invalid input. Please enter a numeric value.")
+        if "-" in stocks:
+            print("Negative values are not allowed.")
